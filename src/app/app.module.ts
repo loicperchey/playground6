3154c16ab88ca5d5576e497ed8797a5dae98b1be
Admin <perchey.loic@gmail.com>
2023-06-21 14:51:45 +0200

    playground6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { IndexComponent } from './index/index.component';
 
+const routes: Routes =[
+{path:'index', component:IndexComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
